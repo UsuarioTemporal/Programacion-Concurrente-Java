37cd2fd3526704ceb9f8f3b9e577aa6bae8e3dbc
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-23 21:46:02 -0500

    Agregando comentario sobre los metodos synchronized

--- src/metodosSynchronized/MetodoSynchronized.java
+++ src/metodosSynchronized/MetodoSynchronized.java
@@ -1,5 +1,13 @@
 package metodosSynchronized;
-
+/*
+ * Garantiza que solo un hilo pueda acceder a un método a la vez.
+ * Si un hilo intenta invocar a ese metodo mientras otro hilo esta dentro de el
+ * el hilo que realiza la llamada quedara bloqueado esperando a que el primer hilo
+ * libere el proceso
+ * 
+ * OJO : puede ser costoso en tiempo de ejecucion 
+ * LOS METODOS SYNCHRONIZED SON MAS LENTOS QUE LOS METODOS NO SINCRONIZADOS
+ * */
 public class MetodoSynchronized {
 	public static void main(String[] args) {
 		
