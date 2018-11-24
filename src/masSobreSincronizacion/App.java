b1e79483401f923c142a57629010784fd33b5ab1
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-23 23:35:27 -0500

    Se comenzará con los hilos

---
+++ src/masSobreSincronizacion/App.java
@@ -0,0 +1,15 @@
+package masSobreSincronizacion;
+
+public class App {
+	public static void main(String[] args) {
+		Thread hilo = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		hilo.start();
+	}
+}
--- src/masSobreSincronizacion/Banco.java
+++ src/masSobreSincronizacion/Banco.java
@@ -1,5 +1,35 @@
 package masSobreSincronizacion;
 
 public class Banco {
-
+	private final double[] cuentas;
+	
+	public Banco() {
+		cuentas=new double[100];
+		for(double i : cuentas) {
+			i=2000;
+		}
+		
+	}
+	
+	
+	public synchronized void transferencia(int cuentaOrigen,int cuentaDestino,double cantidad) {
+		if(cuentas[cuentaOrigen]<cantidad) {
+			return ;
+		}
+		System.out.println(Thread.currentThread()+" ... ");
+		cuentas[cuentaOrigen]-=cantidad;
+		System.out.printf("%10.2f de %d para %d",cantidad,cuentaOrigen,cuentaDestino);
+		cuentas[cuentaDestino]+=cantidad;
+		System.out.printf("Saldo total : %10.2f/n",getGastoTotal());
+		
+	}
+	
+	
+	public double getGastoTotal() {
+		double sumaCuentas = 0;
+		for(double c: cuentas) {
+			sumaCuentas+=c;
+		}
+		return sumaCuentas;
+	}
 }
