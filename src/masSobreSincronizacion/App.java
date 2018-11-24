ea0ff3eb8b7e7e056cfd4af298ea8ec72e518ec2
UsuarioTemporal <thomromanaguilar@gmail.com>
2018-11-24 00:06:54 -0500

    Creando los 100 usuario que seran simulados por los hilos

--- src/masSobreSincronizacion/App.java
+++ src/masSobreSincronizacion/App.java
@@ -2,14 +2,19 @@ package masSobreSincronizacion;
 
 public class App {
 	public static void main(String[] args) {
-		Thread hilo = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
+		Banco banco=new Banco();
+		Thread[] hilos=new Thread[100];
+		for(int i=0;i<hilos.length;i++) {
+			hilos[i]=new Thread(new Runnable() {
 				
-			}
-		});
-		hilo.start();
+				@Override
+				public void run() {
+					
+					
+				}
+			});
+			hilos[i].start();
+		}
 	}
 }
+
